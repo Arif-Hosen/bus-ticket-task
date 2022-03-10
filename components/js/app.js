3b5f6a4d45702394771a1a59778ede2b11949425
Arif-Hosen <aharifhosen1998@gmail.com>
2022-03-10 16:01:05 +0600

    popup-form

---
+++ components/js/app.js
@@ -0,0 +1,9 @@
+document.getElementById("bus-popup").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "flex"
+})
+document.getElementById("hotel-popup").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "flex"
+})
+document.getElementById("close").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none"
+})
